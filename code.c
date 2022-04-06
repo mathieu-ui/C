fd8216e6e47225750ccf686f0943db5970628683
mathieu-iut-beziers <97044385+mathieu-iut-beziers@users.noreply.github.com>
2022-04-06 15:33:52 +0200

    C1

---
+++ code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int i;
+    i = 17;
+    // printf("i vaut %d \n",i); pour linux
+    // printf("i vaut %d \r\n",i); sur windo
+    printf("i vaut %d \r\n",i);
+    //autre exemple : printf("i vaut %d ... %d \r\n",i,a);
+    //un autre printf("i vaut %d \r\n",2*i);
+    return 0;
+}
